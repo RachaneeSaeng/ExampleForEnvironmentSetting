02c2ecc6f26dae56c82fefe2133938f07be86bf9
rachanee.saeng@gmail.com <rachanee.saeng@gmail.com>
2020-10-20 11:31:17 +0700

    initial commit

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+
+namespace WebApplication3
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly VueConfiguration vueConfiguration;
+        private readonly IConfiguration configuration;
+        public ValuesController(IOptions<VueConfiguration> vueConfigOption, IConfiguration config)
+        {
+            vueConfiguration = vueConfigOption.Value;
+            configuration = config;
+        }
+
+        public ActionResult Get()
+        {
+            string url = configuration["vue:URL"];
+            string urlFromConfigurationOption = vueConfiguration.URL;
+
+            return Ok();
+        }
+    }
+}
---
+++ VueConfiguration.cs
@@ -0,0 +1,12 @@
+﻿
+namespace WebApplication3
+{
+    public class VueConfiguration
+    {
+        public string URL { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+    }
+}
